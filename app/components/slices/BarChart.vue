b84567699e667399c00147a25de96dd5e5bec986
Rebecca Jane C <rebjane@Rebeccas-MacBook-Pro.local>
2019-01-08 16:55:03 -0500

    private funds slice minus bor chort

--- app/components/slices/BarChart.vue
+++ app/components/slices/BarChart.vue
@@ -7,12 +7,15 @@
 				<div class="bg"/>
 				<h2 v-if="heading"
 					v-html="heading"/>
-				<a href="#">View PDF</a>
+					<h2 v-html="data.html('vision_opp_pdf_title')"/>
+				<a :href="data.fields.pdf_link_name" target="_blank"><p v-html="data.html('pdf_link_name')"/> </a>
 			</div>
 
 			<div class="title-copy">
+				
 				<h3 v-if="subheading"
 					v-html="subheading"/>
+					<h3 v-html="data.text('bar_chart_title')"/>
 			</div>
 
 			<div class="chart">
@@ -44,6 +47,7 @@ export default {
 	@extend .slice
 	pad(2,0)
 
+
 .wrap
 	display flex
 	flex-wrap wrap
@@ -51,10 +55,12 @@ export default {
 .title-copy
 	order 1
 	width 100%
+	padding-bottom 3rem
 
 	/deep/ h3
 		fs(mp(2))
 		mgn(1,auto)
+		font-family $cormorant-medium
 		text-align center
 
 .title-card
@@ -62,9 +68,13 @@ export default {
 	color $w
 	order 2
 	position relative
-	pad(.5,1,2,0)
+	pad(.5,2,2,0)
 	+below($tablet)
-		pad(.5,0,2,0)
+		pad(.5,1,2,0)
+
+
+	a:before, a:after
+		background none
 
 	/deep/
 		h2, a
@@ -74,15 +84,21 @@ export default {
 		h2
 			fs(mp(2))
 			letter-spacing -(0.32 / 32) * 1em
+			font-family $cormorant-medium
 
 		a
 			display inline-block
 
 	+above($tablet)
-		width (200% / 9)
+		width (200% / 7)
 
 	.bg
-		background $blue
+		background url('../../images/strip-bg.jpg')
+		background-repeat no-repeat
+		background-size cover
+		display inline-block
+		height 100%
+		width auto
 		bottom 0
 		left -($gut * 2em)
 		right 0
