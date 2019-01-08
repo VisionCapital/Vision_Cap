c9d007d9ee44bc4c469db0147656be5c41b72d17
Rebecca Jane C <rebjane@Rebeccas-MacBook-Pro.local>
2019-01-08 10:49:08 -0500

    additional map slice styling

--- app/components/slices/Map.vue
+++ app/components/slices/Map.vue
@@ -11,16 +11,16 @@
 						v-html="subheading"/>
 				</div>
 
-				<div class="cta" v-if="!$store.state.device.mobile"><!-- if it's desktop-->
+				<div class="cta" v-if="$store.state.device.win.x > 1024"><!-- if it's desktop-->
 				<h4 v-html="data.text('map_title')"/>
 				<p v-html="data.text('map_location')"/>
-					<a :href="data.fields.map_link.url" :target="_blank">
+					<a :href="data.fields.map_link.url" target="_blank">
 						<div v-html="data.text('map_link_title')"/>
 						</a>
 					<!-- <core-button label="View Larger Map"/> -->
 				</div>
 
-				<div class="cta-mobile" v-if="$store.state.device.mobile"><!-- if it's mobile-->
+				<div class="cta-mobile" v-if="$store.state.device.win.x < 1024"><!-- if it's mobile-->
 				<h4 v-html="data.text('map_title')"/>
 				<p v-html="data.text('map_location')"/>
 					<!-- <core-button label="View Larger Map"/> -->
@@ -51,30 +51,26 @@ export default {
 
 @import "../../styl/_variables"
 
-
 .wrap
-	width 95%
+
+	width auto
+	display flex
+	background-color $lightgrey
 	margin 0 0 0 auto
-	// compensating for extend .slice  on all the wraps
-	+above(1060px * 1.1)
-		width calc(100vw - (50vw - 1060px / 2))
-	+below($tablet)
-		width 100%
 
 p
-	max-width 100%
+	max-width 80%
 h4
 	color $blue
 
 .map-frame
-	width (600% / 9)
 	width 100%
 .map-ratio
 	overflow hidden
 	padding-bottom 50%
 	position relative
 	width 100%
-	+below($tablet)
+	+below($notebook)
 		padding-bottom 75%
 
 	/deep/ iframe,
@@ -90,29 +86,38 @@ h4
 .wrap
 	display flex
 	justify-content space-between
-	+below($tablet)
+	+below($notebook)
 		flex-direction column
 
 .annotations
 	display flex
+	width auto
 	flex-direction column
 	justify-content center
-	+below($tablet)
+	+above($notebook)//laptop
+		margin-left 11%
+	+above($laptop)//laptop-large
+		margin-left 13%
+	+above($laptop-large)//desktop
+		margin-left 17%
+	+above($desktop)//ultra
+		margin-left 22.5%
+	+below($notebook)
 		text-align center
 	/deep/ h2,
 	/deep/ h3
-		+below($tablet)
+		+below($notebook)
 			fs(30)
 			padding 0 5%
 
 	+above($tablet)
-		min-width 75%
 		text-align left
-		margin-right -50%
+
 
 		.cta
 			pad(1,0)
-			width (200%/ 6)
+			//width (200%/ 6)
+
 
 			.button
 				width 100%
@@ -122,7 +127,6 @@ h4
 		.cta-mobile
 			text-align center
 			display inline-block
-			background-color $lightgrey
 
 	
 </style>
