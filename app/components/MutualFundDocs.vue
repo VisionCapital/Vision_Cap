e8dda47d59d9133b6b66c335e06eb1816f9e269e
dylan <dylanl-j@hotmail.ca>
2019-01-11 14:40:40 -0500

    seperated pdf icon into its own module

--- app/components/MutualFundDocs.vue
+++ app/components/MutualFundDocs.vue
@@ -33,21 +33,8 @@
 			
 			<div class="align-pdf">
 			<a class="pdf" :href="document.pdf.url">
-					<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-						width="50.000000pt" height="72.000000pt" viewBox="0 0 50.000000 72.000000"
-						preserveAspectRatio="xMidYMid meet">
-
-						<g transform="translate(0.000000,72.000000) scale(0.100000,-0.100000)"
-						stroke="none">
-						<path d="M0 360 l0 -360 250 0 250 0 0 235 0 235 -115 0 -115 0 0 125 0 125
-						-135 0 -135 0 0 -360z m420 -5 l0 -25 -170 0 -170 0 0 25 0 25 170 0 170 0 0
-						-25z m0 -120 l0 -25 -170 0 -170 0 0 25 0 25 170 0 170 0 0 -25z m0 -125 l0
-						-30 -170 0 -170 0 0 30 0 30 170 0 170 0 0 -30z"/>
-						<path d="M300 620 l0 -100 102 0 103 0 -100 100 c-55 55 -101 100 -102 100 -2
-						0 -3 -45 -3 -100z"/>
-						</g>
-					</svg>
-					<span>.PDF</span>
+				<pdf-icon class="pdf-icon"/>
+				<span>.PDF</span>
 			</a>
 			</div>
       </div>
@@ -63,10 +50,12 @@
 <script>
 
 import ArrowHead from './svg/ArrowHead.vue';
+import PdfIcon from './svg/PDFIcon.vue';
 
 export default {
 	components: {
-		ArrowHead
+		ArrowHead,
+		PdfIcon
 	},
 	props: [
 		'disclaimer',
@@ -103,7 +92,7 @@ export default {
 .align-pdf
 	pad(0,0,0.2,0)
 
-svg
+.pdf-icon
 	width 1.2rem
 	display: inline-block;
 	vertical-align: top;
