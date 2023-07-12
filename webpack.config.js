e1849be29f575331fecd8a39c9b4a332b52cecf3
DouglasKGlover <douglas.glover@outlook.com>
2023-07-12 14:11:48 -0300

    commented out bit in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,231 +1,216 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 
-var isProd = (process.env.NODE_ENV === 'production');
-var isTest = (process.env.NODE_ENV === 'test');
+var isProd = process.env.NODE_ENV === "production";
+var isTest = process.env.NODE_ENV === "test";
 
-var vueBuild = isProd ? 'vue.min.js' : 'vue.js';
+var vueBuild = isProd ? "vue.min.js" : "vue.js";
 
-var cfgFile = (isProd ? 'prod' : isTest ? 'test' : 'dev') + '.js';
-var config = require(__dirname + '/cfg/' + cfgFile);
+var cfgFile = (isProd ? "prod" : isTest ? "test" : "dev") + ".js";
+var config = require(__dirname + "/cfg/" + cfgFile);
 
-var cssLoader = isProd ? 'css-loader' : 'css-loader?sourceMap';
-var sassLoader = (isProd ? 'sass-loader?' : 'sass-loader?sourceMap&') +
-	'includePaths[]=' + __dirname + '/node_modules/compass-mixins/lib';
+var cssLoader = isProd ? "css-loader" : "css-loader?sourceMap";
+var sassLoader =
+  (isProd ? "sass-loader?" : "sass-loader?sourceMap&") +
+  "includePaths[]=" +
+  __dirname +
+  "/node_modules/compass-mixins/lib";
 
-var filename = isProd ? '[name].[chunkhash]' : '[name]';
+var filename = isProd ? "[name].[chunkhash]" : "[name]";
 
 var commonLib = config.commonLib || [];
 
-var emailConfig = require('./app/email-config.json');
+var emailConfig = require("./app/email-config.json");
 
 var webpackOptions = {
+  context: __dirname + "/app",
 
-	context: __dirname + '/app',
+  entry: {
+    app: ["babel-polyfill", "./js/main.js"],
+    common: ["core-js/es6/promise", "dom4", "vue", "vue-router"].concat(
+      commonLib
+    )
+  },
 
-	entry: {
-		app: [ 'babel-polyfill', './js/main.js' ],
-		common: [
-			'core-js/es6/promise',
-			'dom4',
-			'vue',
-			'vue-router'
-		].concat(commonLib)
-	},
+  output: {
+    path: __dirname + "/dist",
+    publicPath: config.publicPath,
+    filename: "js/" + filename + ".js"
+  },
 
-	output: {
-		path: __dirname + '/dist',
-		publicPath: config.publicPath,
-		filename: 'js/' + filename + '.js'
-	},
+  resolve: {
+    extensions: [".js", ".json"],
+    alias: {
+      vue: "vue/dist/" + vueBuild
+    }
+  },
 
-	resolve: {
-		extensions: [ '.js', '.json' ],
-		alias: {
-			vue: 'vue/dist/' + vueBuild
-		}
-	},
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new webpack.DefinePlugin({
+      __DESCRIPTION: JSON.stringify(config.description),
+      __PATH: JSON.stringify(config.publicPath),
+      __ANALYTICS_UA: JSON.stringify(config.analyticsId),
+      __EMAIL_URL: JSON.stringify(emailConfig.action),
+      __EMAIL_FIELDS: JSON.stringify(emailConfig.fields),
+      __CMS_ADAPTER: JSON.stringify(config.cmsAdapter),
+      __CMS_CONFIG: JSON.stringify(config.cmsConfig)
+    }),
+    new HtmlWebpackPlugin({
+      title: config.title,
+      config: config,
+      template: "index.ejs",
+      inject: "foot",
+      chunksSortMode: "dependency",
+      minify: {
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        collapseBooleanAttributes: true,
+        collapseInlineTagWhitespace: true
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: "favicon.ico", to: "favicon.ico" },
+      { from: "email-config.json", to: "email-config.json" },
+      { from: "manifest.json", to: "manifest.json" },
+      { from: "apple-touch-icon.png", to: "apple-touch-icon.png" }
+    ])
+  ],
 
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new webpack.DefinePlugin({
-			__DESCRIPTION: JSON.stringify(config.description),
-			__PATH: JSON.stringify(config.publicPath),
-			__ANALYTICS_UA: JSON.stringify(config.analyticsId),
-			__EMAIL_URL: JSON.stringify(emailConfig.action),
-			__EMAIL_FIELDS: JSON.stringify(emailConfig.fields),
-			__CMS_ADAPTER: JSON.stringify(config.cmsAdapter),
-			__CMS_CONFIG: JSON.stringify(config.cmsConfig)
-		}),
-		new HtmlWebpackPlugin({
-			title: config.title,
-			config: config,
-			template: 'index.ejs',
-			inject: 'foot',
-			chunksSortMode: 'dependency',
-			minify: {
-				collapseWhitespace: true,
-				conservativeCollapse: true,
-				collapseBooleanAttributes: true,
-				collapseInlineTagWhitespace: true
-			}
-		}),
-		new CopyWebpackPlugin([
-			{ from: 'favicon.ico', to: 'favicon.ico' },
-			{ from: 'email-config.json', to: 'email-config.json' },
-			{ from: 'manifest.json', to: 'manifest.json' },
-			{ from: 'apple-touch-icon.png', to: 'apple-touch-icon.png' }
-		])
-	],
-
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.(vue|js)$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/,
-				options: {
-					fix:true
-				}
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loaders: [ 'babel-loader' ]
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					loaders: {
-						sass: 'vue-style-loader!' + cssLoader + '!' + sassLoader
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: { importLoaders: 1 }
-					},
-					'postcss-loader'
-				]
-			},
-			{
-				test: /\.(scss|sass)$/,
-				loaders: [ cssLoader, sassLoader ]
-			},
-			{
-				test: /\.styl(us)?$/,
-				use: [
-					'vue-style-loader',
-					'css-loader',
-					'stylus-loader'
-				]
-			},
-			{
-				test: /\.html$/,
-				loader: 'file-loader?name=[name].[ext]'
-			},
-			{
-				test: /\.pug$/,
-				loader: 'pug-plain-loader'
-			},
-			{
-				test: /\.(eot|ttf|woff|woff2|otf|fnt)$/,
-				loader: 'file-loader?hash=sha512&digest=hex&name=fonts/[hash].[ext]'
-			},
-			{
-				test: /\.(mp4|m4v)$/,
-				loader: 'file-loader?hash=sha512&digest=hex&name=video/[hash].[ext]'
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/,
-				loaders: [
-					'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
-					{
-						loader: 'image-webpack-loader',
-						query: {
-							optipng: {
-								bypassOnDebug: false,
-								optimizationLevel: 0
-							}
-						}
-					}
-				]
-			},
-			{
-				test: /\.(frag|vert|glsl)$/,
-				loaders: [
-					'raw-loader',
-					'glslify-loader'
-				],
-				exclude: /node_modules/
-			}
-		]
-	}
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(vue|js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {
+            sass: "vue-style-loader!" + cssLoader + "!" + sassLoader
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: [cssLoader, sassLoader]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: ["vue-style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]"
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-plain-loader"
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|otf|fnt)$/,
+        loader: "file-loader?hash=sha512&digest=hex&name=fonts/[hash].[ext]"
+      },
+      {
+        test: /\.(mp4|m4v)$/,
+        loader: "file-loader?hash=sha512&digest=hex&name=video/[hash].[ext]"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loaders: [
+          "file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]"
+          // {
+          // 	loader: 'image-webpack-loader',
+          // 	query: {
+          // 		optipng: {
+          // 			bypassOnDebug: false,
+          // 			optimizationLevel: 0
+          // 		}
+          // 	}
+          // }
+        ]
+      },
+      {
+        test: /\.(frag|vert|glsl)$/,
+        loaders: ["raw-loader", "glslify-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
 
 if (isProd) {
+  webpackOptions.plugins.push(new webpack.HashedModuleIdsPlugin());
 
-	webpackOptions.plugins.push(
-		new webpack.HashedModuleIdsPlugin()
-	);
+  webpackOptions.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common"
+    })
+  );
 
-	webpackOptions.plugins.push(
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'common'
-		})
-	);
+  webpackOptions.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "runtime"
+    })
+  );
 
-	webpackOptions.plugins.push(
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'runtime'
-		})
-	);
-
-	if (config.prerenderPaths && config.prerenderPaths.length) {
-		var PrerenderSpaPlugin = require('prerender-spa-plugin');
-		webpackOptions.plugins.push(new PrerenderSpaPlugin(
-			__dirname + '/dist',
-			config.prerenderPaths
-		));
-	}
-
+  if (config.prerenderPaths && config.prerenderPaths.length) {
+    var PrerenderSpaPlugin = require("prerender-spa-plugin");
+    webpackOptions.plugins.push(
+      new PrerenderSpaPlugin(__dirname + "/dist", config.prerenderPaths)
+    );
+  }
 } else {
+  webpackOptions.devServer = {
+    host: "0.0.0.0",
+    port: 8080,
+    disableHostCheck: true,
+    inline: true,
+    hot: true,
 
-	webpackOptions.devServer = {
-		host: '0.0.0.0',
-		port: 8080,
-		disableHostCheck: true,
-		inline: true,
-		hot: true,
+    // Browser will automatically open page. Comment to disable.
+    // open               : false,
 
-		// Browser will automatically open page. Comment to disable.
-		// open               : false,
+    historyApiFallback: true,
 
-		historyApiFallback: true,
+    // webpack-dev-middleware options
+    quiet: false,
+    noInfo: false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  };
 
-		// webpack-dev-middleware options
-		quiet: false,
-		noInfo: false,
-		watchOptions: {
-			aggregateTimeout: 300,
-			poll: 1000
-		}
-	};
+  var DashboardPlugin = require("webpack-dashboard/plugin");
 
-	var DashboardPlugin = require('webpack-dashboard/plugin');
-
-	webpackOptions.plugins.push(new DashboardPlugin());
-	webpackOptions.plugins.push(new webpack.NamedModulesPlugin());
-	webpackOptions.plugins.push(new webpack.HotModuleReplacementPlugin());
-
+  webpackOptions.plugins.push(new DashboardPlugin());
+  webpackOptions.plugins.push(new webpack.NamedModulesPlugin());
+  webpackOptions.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 module.exports = webpackOptions;
