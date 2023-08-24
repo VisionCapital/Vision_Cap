0d13b5178fe08d68d370b16075929fde49152182
Sydney Nguyen <sydney2004nguyen@gmail.com>
2023-08-23 21:10:52 -0400

    Test 2 for Hero Button

--- app/components/slices/test_page.vue
+++ app/components/slices/test_page.vue
@@ -1,112 +1,59 @@
 <template>
-	<div class="hero">
-
-		<div class="wrap">
-			<div class="copy" v-if="data && data.fields">
-
-				<div class="hero-image" v-if="data.fields.image.url">
-					<img :src="data.image('image')"/>
-				</div>
-
-				<h1 v-if="data.fields.title1.length"
-					v-html="$cms.textField(data.fields.title1)"/>
-
-				<div v-if="data.fields.paragraph.length"
-					v-html="$cms.htmlField(data.fields.paragraph)"/>
-
-                <router-link :to="data.fields.button_link">
-                    <button class="accept-button">
-                        <span class="button-label" v-html="$cms.textField(data.fields.button_label)"/>
-                    </button>             
-                </router-link>
-
-			</div>
-		</div>
-
-	</div>
-</template>
-
-<script>
-
-import airprops from '../../mixins/airprops';
-
-export default {
-
-	mixins: [ airprops ]
-
-};
-
-</script>
-
-<style lang="stylus" scoped>
-
-@import "../../styl/_variables"
-
-.hero
-	@extend $slice
-	background none
-	pad(3,0,1)
-	position relative
-
-	+above($notebook)
-		pad(4,0,2)
-
-.wrap
-	color $w
-	position relative
-
-	/deep/
-		h1
-			color $w
-			// fs(82)
-			mgn(1,0,2)
-
-			+below($tablet)
-				fs(60)
-				line-height (73/60)
-
-			transition line-height 0.5s, opacity 0.5s
-			transition-delay 0.5s
-
-			.v-enter &
-				line-height 1.5
-				opacity 0
-
-		p
-			fs(16)
-			font-smoothing()
-
-			+below($tablet)
-				fs(12)
-				line-height (24/12)
-
-			transition opacity 0.5s
-			transition-delay 0.5s
-
-			.v-enter &
-				opacity 0
-
-.hero-image
-	pad(2,0,1)
-
-	+above($tablet)
-		pad(1, 3)
-
-		/deep/ img
-			mgn(0,auto)
-
-	+above($notebook)
-		pad(1, 5)
-
-.accept-button
-    background $blue
-    color white
-    border none
-    padding 10px 20px
-    cursor pointer
-
-.button-label
-    font-size 16px
-    font-weigth bold
-
-</style>
+    <div class="hero">
+      <div class="hero-image" v-if="data && data.fields.image.url">
+        <img :src="data.image('image')" />
+      </div>
+  
+      <h1 class="hero-title" v-if="data.fields.title1.length" 
+      v-html="$cms.text(data.fields.title1)" />
+  
+      <router-link :to="data.fields.button_link.slug">
+        <button class="accept-button">
+          <span class="button-label">Accept</span>
+        </button>
+      </router-link>
+    </div>
+  
+    <div class="paragraph-section">
+      <div class="wrap">
+        <div class="copy" v-if="data && data.fields">
+          <div v-if="data.fields.paragraph.length" v-html="$cms.text(data.fields.paragraph)" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import airprops from '../../mixins/airprops';
+  
+  export default {
+  	mixins: [ airprops ]
+  };
+  </script>
+  
+  <style lang="stylus" scoped>
+    .hero
+      background-color blue
+      color white
+      padding 20px
+  
+      .hero-image img
+        max-width 100%
+        height auto
+  
+      .hero-title
+        font-size 24px
+        margin-top 20px
+  
+      .accept-button
+        background-color white
+        color blue
+        border none
+        padding 10px 20px
+        cursor pointer
+  
+    .paragraph-section
+      background-color white
+      padding 20px
+  </style>
+  
